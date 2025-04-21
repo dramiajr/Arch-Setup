ae58940e0da16598068be818c1924f476ac3bd1a
David Ramia Jr <davidramiajr@gmail.com>
2025-04-21 17:51:27 -0500

    Update setwall.sh

--- setwall.sh
+++ setwall.sh
@@ -27,14 +27,15 @@ fi
 # Copy wallpaper image to Rofi Directory
 ROFI_DIR="$HOME/.config/rofi"
 IMAGE="image.jpg"
-UPDATE_ROFI="$HOME/.config/rofi/$IMAGE"
+UPDATE_ROFI="$ROFI_DIR/$IMAGE"
 
+# Resize Image
 if [ -d $ROFI_DIR ]; then
-    sudo cp $WALLPAPER $UPDATE_ROFI
+    sudo magick $WALLPAPER -resize 854x360 -gravity center $UPDATE_ROFI
     echo "Rofi Image Updated"
 else
     echo "Rofi Directory Not Found: $ROFI_DIR"
-fi 
+fi  
 
 #################
 # Applying Pywal:
